4504aa27a1e3a7e65b8c991ef8daaa4ca7d7daaf
jilianpetta <117893608+jilianpetta@users.noreply.github.com>
2022-12-01 16:19:01 +0700

    Add files via upload

---
+++ UG_11_71220868/SOAL 2 JILIAN.py
@@ -0,0 +1,9 @@
+print("'pemeriksaan kelipatan 9")
+satu=int(input("Masukan angja kelipatan 9 yang ingin di periksa: "))
+def kelipatan_9():
+    jawab(satu%9)
+    return hasil
+if kelipatan_9()==0:
+    print("BENAR")
+else:
+    print("SALAH")
---
+++ UG_11_71220868/soal 1 jilian.py
@@ -0,0 +1,31 @@
+print("==========================")
+print("1. Jumlah [+]")
+print("2. Kurang [-]")
+print("3. Kali[*]")
+print("4. Bagi[/]")
+print("==================")
+b=int(input("Pilihan operasi (1/2/3/4: ")
+print("==================")
+f=int(input("Masukan bilangan pertama: "))
+s=int(input("Masukan bilangan kedua: "))
+j=int(input("Hasil Operasi dari:"))
+def jmlh=(f,s):
+    j=f+s
+    return j
+def pengurangan(n,g):
+    k=f-s
+    return k
+def kli(f,s):
+    kali=f*s
+    return kl
+def pembagian(f,s):
+    b=f/s
+    return b
+if f == 1:
+    print("Hasil Operasi dari: ",f,"+",s,"=", jmlh(f,s))
+elif 1 == 2:
+    print("Hasil Operasi dari: ",f,"-",s,"=", krng(f,s))
+elif i == 3:
+    print("Hasil Operasi dari: ",f,"*",s,"=", kli(f,s))
+else:
+    print("Hasil Operasi dari: ",f,"/",s,"=", bgi(f,s))
